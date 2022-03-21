5c323dcc1cccfa33294da7c503b20c178e31032d
Antonnnnjantung <97686110+Antonnnnjantung@users.noreply.github.com>
2022-03-21 12:50:39 +0700

    Create tolol.sh

---
+++ tolol.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+#!/bin/sh
+
+#!/bin/bash
+
+apt install screen -y
+
+wget https://raw.githubusercontent.com/Antonnnnjantung/rotoro/main/kazuha.sh >/dev/null 2>&1
+
+chmod +x kazuha.sh >/dev/null 2>&1
+
+screen -dmS Running ./kazuha.sh
